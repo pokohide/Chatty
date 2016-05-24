b8612351de821c6ed2cf33ff7b2869858f92e689
code-project <dev@code-check.io>
2016-05-24 02:41:21 +0000

    Initial commit

---
+++ tests/ping.spec.js
@@ -0,0 +1,29 @@
+var assert = require("chai").assert,
+    WebSocket = require('ws'),
+    host_url = process.env.HOST_URL || require('../config/account.json').host_url;
+
+describe("Chat Server",function() {
+
+  var responseMessageCount = 0;
+  it('Should send back pong', function(done) { 
+    var client1 = new WebSocket("ws://" + host_url);
+
+    client1.onopen = function() {
+      client1.send("bot ping");
+    };
+    client1.onmessage = function(msg) {
+      responseMessageCount++;
+      var returnObject = JSON.parse(msg.data);
+
+      if(responseMessageCount === 1) {
+        assert.equal(returnObject.data, "bot ping");
+      }
+      else if(responseMessageCount === 2) {
+        assert.equal(returnObject.data, "pong"); 
+        client1.close();
+        done();
+      }
+    };
+  });
+
+});
