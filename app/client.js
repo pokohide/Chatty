3154947ac63344c9e2b5b77d1363377802eb63f1
pokohide <hyde31415@gmail.com>
2016-05-26 02:46:58 +0900

    socketの設定変更

--- app/client.js
+++ app/client.js
@@ -10,33 +10,32 @@ $(function() {
     '#c0392b', '#bdc3c7', '#7f8c8d'
   ]
 
-  const $window = $(window)
   const $timeline = $('.timeline__list')
-  const $chatForm = $('.m')
+  const $chatInput = $('.mainArea__form--input')
+  const handle, handleColor
 
-  const $loginPage = $('.login__page')
-  const $chatPage = $('.chat__page')
+  var connected = false
 
   var lastTypingTime;
-  var connected = false
-  ,   typing = false
+  var typing = false
 
   const socket = io()
   var $dimmed = $('.mainArea__dimmed')
-  //$('body').append($dimmed)
 
   // ハンドルネームを設定
   $('.mainArea__handle--submit').on('click', function(e) {
-    const handle = $('.mainArea__handle--input').val()
-    console.log(handle)
-    if( handle ) {
-      $dimmed.fadeOut()
-      $('.mainArea__handle').fadeOut()
-      socket.emit('add user', handle)
-    }
+    setHandle()
     e.preventDefault()  
   })
 
+  // チャットする
+  $('.mainArea__handle--submit').on('click', function(e) {
+
+    e.preventDefault()
+  })
+
+
+  // Chattyの人数を表示
   function addMembersMessage(data) {
     const message = ''
     if( Number(data.userCount) == 1) {
@@ -44,66 +43,200 @@ $(function() {
     } else {
       message += 'このチャットルームの参加者は' + data.userCount + '人です。'
     }
-    log(message)
+    roomMessage(message)
   }
 
+  // ハンドルネームを設定
+  function setHandle() {
+    handle = $('.mainArea__handle--input').val()
+    if(handle) {
+      $dimmed.fadeOut()
+      $('.mainArea__handle').fadeOut()
+      $chatForm.focus()
+      socket.emit('add user', handle)
+
+      const index = Math.floor( Math.random() * COLORS.length )
+      handleColor = COLORS[index]
+    }
+  }
+
+  // メッセージを送る
+  function sendMessage() {
+    const message = $chatInput.val()
+    if( message && connected ) {
+      $chatInput.val('')
+      addChatMessage({
+        handle: handle,
+        message: message
+      })
+      socket.emit('new message', message)
+    }
+  }
+
+  // ユーザのメッセージを表示
+  function addChatMessage(data, options) {
+    //   var $typingMessages = getTypingMessages(data);
+    //   options = options || {};
+    //   if ($typingMessages.length !== 0) {
+    //     options.fade = false;
+    //     $typingMessages.remove();
+    //   }
+    const $handleDiv = $('<span class="timeline__item--handle" />').text(data.handle).css('color', handleColor)
+    const $msgBodyDiv = $('<span class="timeline__item--message" />').text(data.message)
+
+    //const typingClass = data.typing ? 'typing' : ''
+    const $msgDiv = $('<li class="timeline__list--item" />')
+      .data('handle', data.handle)
+      //.addClass(typingClass)
+      .append($handleDiv, $msgBodyDiv)
+    addMessageToTimeline($msgDiv, options)
+  }
+
+  // roomメッセージ追加
+  function roomMessage(message, options) {
+    const $msg = $('<li>').addClass('roomMessage').text(message)
+    addMessageToTimeline($msg, options)
+  }
+
+  // botメッセージ追加
+  function botMessage(message, options) {
+    const $msg = $('<li>').addClass('botMessage').text(message)
+    addMessageToTimeline($msg, options)
+  }
+
+  // メッセージをタイムラインに追加
+  function addMessageToTimeline(msg, options) {
+    $msg = $(msg)
+
+    if(!options) options = {}
+    if(typeof options.fade == 'undefined') options.fade = true
+    if(typeof options.prepend == 'undefined') options.prepend = true
+
+    if(options.fade) {
+      $msg.hide().fadeIn(FADE_TIME)
+    }
+    if(options.prepend) {
+      $timeline.prepend($msg)
+    } else {
+      $timeline.append($msg)
+    }
+    $timeline[0].scrollTop = $timeline[0].scrollHeight
+  }
+
+
+  // チャット入力中
+  $chatInput.on('input', function() {
+    // nowTyping()
+  })
+
+
+  // キーボード入力検知
+  $(window).on('keydown', function(e) {
+    // Enterキーで入力可能に
+    if(e.keyCode == 13) {
+      if(handle) {
+        sendMessage()
+        // socket.emit('stop typing')
+        // typing = false
+      } else {
+        setHandle()
+      }
+    }
+  })
+
+
+  // ログイン
+  socket.on('connected', function(data) {
+    connected = true
+    const message = 'Chattyへようこそ'
+    log(message, {prepend: true})
+    addMembersMessage(data);
+  })
+
+  // 新しいメッセージ
+  socket.on('new message', function(data) {
+    addChatMessage(data)
+  })
+
+  // ユーザ参加通知
+  socket.on('user joined', function(data) {
+    roomMessage(data.handle + 'が参加しました。')
+    addMembersMessage(data)
+  })
+
+  // ユーザ離席通知
+  socket.on('user left', function(data) {
+    roomMessage(data.handle + 'が離席しました。')
+    addMembersMessage(data)
+    // removeChatTyping(data)
+  })
+
+  socket.on('typing', function(data) {
+    console.log('now typing')
+    // addChatTyping(data)
+  })
+
+  socket.on('stop typing', function(data) {
+    console.log('remove typing')
+    // removeChatTyping(data)
+  })
 
 });
 
 
 
-// 'use strict';
+  // Adds the visual chat typing message
+  // function addChatTyping (data) {
+  //   data.typing = true;
+  //   data.message = 'is typing';
+  //   addChatMessage(data);
+  // }
 
-// var ws = new WebSocket('ws://localhost:3000/');
+  // // Removes the visual chat typing message
+  // function removeChatTyping (data) {
+  //   getTypingMessages(data).fadeOut(function () {
+  //     $(this).remove();
+  //   });
+  // }
 
-// $(function () {
-//   $('form').submit(function(){
-//     var $this = $(this);
-//     // ws.onopen = function() {
-//     //   console.log('sent message: %s', $('#m').val());
-//     // };
-//     ws.send($('#m').val());
-//     $('#m').val('');
-//     return false;
-//   });
-//   ws.onmessage = function(msg){
-//     var returnObject = JSON.parse(msg.data);
-//     $('#messages').append($('<li>')).append($('<span id="clientId">').text(returnObject.id)).append($('<span id="clientMessage">').text(returnObject.data));
-//   };
-//   ws.onerror = function(err){
-//     console.log("err", err);
-//   };
-//   ws.onclose = function close() {
-//     console.log('disconnected');
-//   };
+  // Adds a message element to the messages and scrolls to the bottom
+  // el - The element to add as a message
+  // options.fade - If the element should fade-in (default = true)
+  // options.prepend - If the element should prepend
+  //   all other messages (default = false)
 
 
-//   var wsUri = "ws://localhost:9999/echo";
-//   var output;
+  // // Prevents input from having injected markup
+  // function cleanInput (input) {
+  //   return $('<div/>').text(input).text();
+  // }
 
-//   function init() {
-//     output = document.getElementById('messages');
-//     testWebSocket();
-//   }
+  // // Updates the typing event
+  // function updateTyping () {
+  //   if (connected) {
+  //     if (!typing) {
+  //       typing = true;
+  //       socket.emit('typing');
+  //     }
+  //     lastTypingTime = (new Date()).getTime();
 
-//   function testWebSocket() {
-//     ws = new WebSocket(wsUri);
-//     ws.onopen = function(e) {
-//       console.log('opened');
-//     }
-//     ws.onclode = function(e) {
-//       console.log('closed');
-//     }
-//     ws.onmessage = function(e) {
-//       console.log('aaa');
-//     }
-//     ws.onerror = function(e) {
-//       console.log('error');
-//     }
-//     window.addEventListener("load", init, false);
+  //     setTimeout(function () {
+  //       var typingTimer = (new Date()).getTime();
+  //       var timeDiff = typingTimer - lastTypingTime;
+  //       if (timeDiff >= TYPING_TIMER_LENGTH && typing) {
+  //         socket.emit('stop typing');
+  //         typing = false;
+  //       }
+  //     }, TYPING_TIMER_LENGTH);
+  //   }
+  // }
 
-//   }
-// });
+  // Gets the 'X is typing' messages of a user
+  // function getTypingMessages (data) {
+  //   return $('.typing.message').filter(function (i) {
+  //     return $(this).data('username') === data.username;
+  //   });
+  // }
 
 
 
