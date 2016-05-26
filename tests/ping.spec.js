04b9ef253dd00616b00669f731350f41ea8f3d67
code-project <dev@code-check.io>
2016-05-26 05:34:36 +0000

    Overwrite original challenge files

--- tests/ping.spec.js
+++ tests/ping.spec.js
@@ -1,12 +1,13 @@
 var assert = require("chai").assert,
     WebSocket = require('ws'),
+    setWsProtocol = require('./utils').setWsProtocol,
     host_url = process.env.HOST_URL || require('../config/account.json').host_url;
 
 describe("Chat Server",function() {
 
   var responseMessageCount = 0;
   it('Should send back pong', function(done) { 
-    var client1 = new WebSocket("ws://" + host_url);
+    var client1 = new WebSocket(setWsProtocol(host_url));
 
     client1.onopen = function() {
       client1.send("bot ping");
